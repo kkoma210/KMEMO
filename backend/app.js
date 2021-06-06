719b83713e5df4d0b88612718f77b531ba572415
kkoma210 <kkoma@sparcs.org>
2021-06-06 22:46:36 +0900

    Change db url

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,11 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path = require('path');
-var router = express.Router();
 
 var diaryRouter = require('./routes/diary');
 
 var app = express();
 
-//app.use(express.static(path.join(__dirname, '../fronent/build')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,7 +19,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/myDiary", {
+mongoose.connect("mongodb://whale.sparcs.org:45921/myDiary", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -30,10 +28,6 @@ const db = mongoose.connection;
 db.once("open", function() {
   console.log("DB connected!");
 });
-
-// router.get('/', function(req, res, next){
-//   res.send(express.static(path.join(__dirname, '../frontend/build/index.html')));
-// });
 
 app.use('/diary', diaryRouter);
 
