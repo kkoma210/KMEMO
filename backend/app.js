056cf5b4f0a73cb7e87083815d67885af08dc8cc
kkoma210 <kkoma@sparcs.org>
2021-06-06 20:36:17 +0900

    Try something to deploy

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,14 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var path = require('path');
+var router = express.Router();
 
 var diaryRouter = require('./routes/diary');
 
 var app = express();
 
+//app.use(express.static(path.join(__dirname, '../fronent/build')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,6 +30,11 @@ const db = mongoose.connection;
 db.once("open", function() {
   console.log("DB connected!");
 });
+
+// router.get('/', function(req, res, next){
+//   res.send(express.static(path.join(__dirname, '../frontend/build/index.html')));
+// });
+
 app.use('/diary', diaryRouter);
 
 module.exports = app;
